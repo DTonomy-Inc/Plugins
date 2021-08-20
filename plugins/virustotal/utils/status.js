f8a7ee9d332b1c838f7efbb2348755d02c45834c
jdtonomy <jasmine@dtonomy.com>
2021-08-19 22:59:29 -0700

    update

--- plugins/virustotal/utils/status.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	READY : {fill: 'blue', shape: 'dot', text: 'ready'},
-	PROCESSING : {fill: 'yellow', shape: 'dot', text: 'processing'},
-	DONE : {fill: 'green', shape: 'dot', text: 'done'},
-	ERROR : {fill: 'red', shape: 'dot',text: 'error'},
-	NONE : {}
-}
